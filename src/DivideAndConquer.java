f6a3aa9fbd7710a266790b41e02e200ca995c0f4
mmolteratx <32653343+mmolteratx@users.noreply.github.com>
2020-11-26 17:31:07 -0600

    Initial commit

---
+++ src/DivideAndConquer.java
@@ -0,0 +1,2 @@
+public class DivideAndConquer {
+}
---
+++ src/Node.java
@@ -0,0 +1,27 @@
+// 2-d point represented as a node
+
+public class Node {
+    private int x;
+    private int y;
+
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
